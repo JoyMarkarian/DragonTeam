691fcd594af15625d9b4faedc30cc5a4d571dbc5
Tormo <sylvaain11@hotmail.fr>
2022-09-30 09:41:00 +0200

    header work

---
+++ script.js
@@ -0,0 +1,13 @@
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+function openNav() {
+  sidenav.classList.add("active");
+}
+function closeNav() {
+  sidenav.classList.remove("active");
+}
